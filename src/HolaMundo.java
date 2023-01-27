9a4b71e9cff1ce2b36390fc9d7bd40fd5421e4f6
Kenny Molina <kjmolina@live.com>
2023-01-27 09:03:19 -0600

    first commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    /*
+    sout shortcut print output
+    main genera el metodo de abajo
+    * */
+    public static void main(String[] args) {
+        System.out.println("hola kenny");
+    }
+}
