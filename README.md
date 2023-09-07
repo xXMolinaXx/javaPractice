3a62b73d4d1d45727ae9d930c9a87666954500ee
Kenny Molina <kjmolina@live.com>
2023-09-07 15:02:57 -0600

    add image

---
+++ README.md
@@ -0,0 +1,27 @@
+# Apuntes de java
+
+### atajos
+- sout: system.out.println(")
+
+### conevencion de nombres
+
+- UpperCammelCase:clases
+- lowerCammelCase: variables
+
+### JAR
+
+- codigo de compliacion java
+-  file -> project structure -> artifac 
+- selecionar el mas
+- clieckear JAR -> modules with dependencies
+- despues con build -> build artifact -> build
+- se crea esta carpeta y con java --jar <nombre archivo> asi se ejecuta los programas de java
+- 
+## JAVA DOC
+- herramienta para hacer documentacion
+- Para que el IDE muestre la descripción y documentación de las funciones debemos entrar a IntelliJ IDEA > Preferences > Editor > General > Code Complettion y habilitar la opción de Show the documentarion popup.
+
+### extras
+- jshell es la terminal de java para probar codigo sin necesidad de crear un proyecto
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     *Descripcion: funcion ejemplo java doc
+     *
+     * @param a numero entero
+     * @return no regresa nada
+     * */
+    public static  void funcsi (int a){
+        System.out.println(a + 1);
+        return;
+    }
     public static void main(String[] args) {
-        // atajos
-        // sout: system.out.println(")
+        // declarando variables
+        int speed = 0;
+        System.out.println(speed);
+        int salary = 1000;
+        String nombre = "kenny";
+        System.out.println(nombre);
+        int  n= 1234567890;// solo puede usar 10 digitos
+        // int  n= 12345678901; da error porque tiene 11 digitos
+        // tipo de datos numericos,
+        byte b = 1;
+        short s = 1;
+        int i = 1;
+        long l = 1L;
+        // numeros con decimal
+        float f = 1.1F;
+        double d =1.1;
+        // texto
+        // para char es comilla simple y string comilla doble
+        char c = 'a';
+        boolean yes = true;
+        boolean no = false;
+        // nuevas vaiables de java
+
+        var kenny = "kenny";
+        double sqrv = Math.sqrt(2);
+        System.out.println(sqrv);
+
+        // casteo
+        double monthlyDOgs = 30.0/12.0;
+        int monthlyINt = (int) monthlyDOgs;
+        System.out.println(monthlyINt);
+
         System.out.println("escribe en el teclado");
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
@@ -16,5 +56,6 @@ public class Main {
         System.out.println(value);
         System.out.println(newHolaMundo);
         System.out.println(Math.sqrt(4));
+        funcsi(5);
     }
 }
